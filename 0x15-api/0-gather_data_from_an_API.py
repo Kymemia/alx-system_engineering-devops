68551e654cc50f414cf7038232229eb61bb7ac08
Kymemia <kimemiachristopherr@gmail.com>
2024-05-21 04:37:23 +0300

    gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""This is a python script that returns information
+about his/her TODO list progress using a REST API"""
+
+
+import requests
+import sys
+
+
+def fetch_user_data(user_id):
+    """fetch user data"""
+    response = requests.get
+    (f"https://jsonplaceholder.typicode.com/users/{user_id}")
+    response.raise_for_status()
+    return response.json()
+
+
+def fetch_todos():
+    """fetch todos data"""
+    response = requests.get("https://jsonplaceholder.typicode.com/todos")
+    response.raise_for_status()
+    return response.json()
+
+
+def main(user_id):
+    """displays TODO list progress"""
+    user_data = fetch_user_data(user_id)
+    todos_data = fetch_todos()
+    user_name = user_data["name"]
+    total_tasks = sum(1 for task in todos_data if task["userId"] == user_id)
+    completed_tasks = [task["title"] for task in todos_data
+                       if task["userId"] == user_id and task["completed"]]
+
+    print(f"Employee {user_name} is done"
+          "with tasks({len(completed_tasks)}/{total_tasks}):")
+    for title in completed_tasks:
+        print("\t ", title)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer")
+        sys.exit(1)
+
+    main(employee_id)
